a8d8fc72d885a37f344c23a175247fe060ec8369
harry <harry@harry-PC>
2016-05-15 09:25:01 +0100

    Sound added, get keypressed moved to octopus

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import pygame
 import random
 import Octopus
-
-
+import Powerup
 __version__ = '2.0.0'
+
+pygame.mixer.pre_init(44100, -16, 1, 1024)
+pygame.init()
+pygame.mixer.init() #Init the music package so that we can use it
+
 
 clock = pygame.time.Clock()
 display = pygame.display.set_mode((1280,720))
@@ -13,39 +17,54 @@ backdrop = pygame.image.load('Backdrop.png').convert()
 
 player = Octopus.octopus()
 
+InkPots = Powerup.InkPots(player)
+Coins = Powerup.Coins(player)
+
+def loadandplaymusic():
+    
+    pygame.mixer.music.load('Main game music.ogg')
+    pygame.mixer.music.play(-1, 0) 
+
+def updatepowerups():
+    
+    InkPots.update(display)
+    Coins.update(display)
+loadandplaymusic()
 #Game loop
 while True:
     
     display.blit(backdrop, (0,0))
     
-    move = False
-    direction = ''
-    
+    #===========================================================================
+    # DELTA TIME STUFF
+    #===========================================================================
     deltatime = clock.tick(60)
     deltatime = float(deltatime) / 100
     
+    #===========================================================================
+    # LAG GENERATION
+    #===========================================================================
     #Lag generation!!!! For testing le delta time
     #for i in range(1,100000):
         
         #random.randrange(1,100)
     
+    #===========================================================================
+    # EVENT COLLECTION
+    #===========================================================================
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-    
-    '''
-    This bit needs revising
-    and intergrating into octopus
-    just the keypress should be fed into octopus.
-    '''
     keypressed = pygame.key.get_pressed()
     
+    #===========================================================================
+    # UPDATES
+    #===========================================================================
     player.update(keypressed, deltatime, display)
     
-    
-    
-  
+    updatepowerups()
+
     
     pygame.display.flip()
     
